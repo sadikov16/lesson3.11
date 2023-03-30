71a8d130139a1d72a97122054d09534b5b12e551
sadikov16 <sodiqovu880@gmail.com>
2023-03-30 14:09:47 +0500

    Main

--- main.js
+++ main.js
@@ -1,64 +1,76 @@
 let elList = document.querySelector("#list")
 let elForm = document.querySelector("#form")
-let elDeleteBtn = document.querySelectorAll("#deleteBtn")
 
-let todosArr = [
-]
+let todosArr = getLocalStorage() || [];
 
-elForm.addEventListener("submit", evt => {
+elForm.addEventListener("submit" , evt =>{
     evt.preventDefault()
     let {todo} = evt.target.elements
     
-    let newObject = {
+    let newObj = {
         id: todosArr.length + 1,
         todo: todo.value.trim(),
-        isColplete: false
+        isComplate: false
     };
-    todosArr.push(newObject)
-    renderingFunc(todosArr, elList);
+    todosArr.unshift(newObj);
+    saveLocalStorage(todosArr);
+    renderignFunc(todosArr , elList);
     todo.value = null
 })
 
-
-function renderingFunc(array,element){
-    element.innerHTML = null;
-    for(let i = 0; i < array.length; i++){
+function renderignFunc(array , element) {
+    element.innerHTML = null
+    for (let i = 0; i < array.length; i++) {
         let newLi = document.createElement("li");
         let newCheckbox = document.createElement("input");
         let newP = document.createElement("p");
-        let newBtn = document.createElement("button");
+        let newButton = document.createElement("button");
         
-        if (array[i].isColplete){
-            newCheckbox.setAttribute("checked", "false")
+        if(array[i].isComplate){
+            newCheckbox.setAttribute("checked" , "true");
         }
         
         newLi.setAttribute("class", "flex gap-16");
         newCheckbox.setAttribute("type", "checkbox");
-        newBtn.setAttribute("class", "bg-red-100");
-        newCheckbox.dataset.todoId = array[i].id;
-        newBtn.dataset.todoId = array[i].id;
+        newButton.setAttribute("class", "delbtn");
         
-        newBtn.addEventListener("click", evt => {
-            let btnId = evt.target.dataset.todoId;
-            let founIndex = todosArr.findIndex(item => item.id == btnId)
-            todosArr.splice(founIndex, 1)
-            renderingFunc(todosArr, elList)
+        newButton.dataset.todoID = array[i].id;
+        newCheckbox.dataset.todoID = array[i].id;
+        
+        newButton.addEventListener("click" , evt => {
+            let li = evt.target.parentNode;
+            let ul = li.parentNode;
+            let btnID = evt.target.dataset.todoID;
+            let found = todosArr.findIndex((item) => item.id == btnID);
+            todosArr.splice(found , 1);
+            saveLocalStorage(todosArr);
+            ul.removeChild(li)
         })
         
-        newCheckbox.addEventListener("click", (evt) => {
-            let btnId = evt.target.dataset.todoId;
-            let foundTodo = todosArr.find((item) => item.id == btnId);
-            foundTodo.isComplate = !foundTodo.isComplate;
-            renderingFunc(todosArr, elList);
-          });
-
-        newP.textContent = (array[i].todo);
-        newBtn.textContent = "Delete"
+        newCheckbox.addEventListener("click" , evt => {
+            let btnID = evt.target.dataset.todoID;
+            let foundtodo = todosArr.find((item) => item.id == btnID);
+            foundtodo.isComplate = !foundtodo.isComplate;
+            saveLocalStorage(todosArr);
+            renderignFunc(todosArr , elList);
+        })
         
-        newLi.append(newCheckbox)
-        newLi.append(newP)
-        newLi.append(newBtn)
+        newP.textContent = array[i].todo;
+        newButton.textContent = "Delete";
         
-        element.append(newLi)
+        newLi.append(newCheckbox);
+        newLi.append(newP);
+        newLi.append(newButton);
+        
+        element.append(newLi);
     }
+}
+renderignFunc(todosArr , elList);
+
+function saveLocalStorage(value){
+    window.localStorage.setItem("todos", JSON.stringify(value));
+}
+
+function getLocalStorage() {
+    return JSON.parse(window.localStorage.getItem("todos"));
 }
